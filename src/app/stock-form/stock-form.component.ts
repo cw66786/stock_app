d49de4bf74a1f3bec8e33a76911035f2e872bebc
Craig Wilson <cw66786@gmail.com>
2023-03-10 17:40:00 -0600

    break

--- src/app/stock-form/stock-form.component.ts
+++ src/app/stock-form/stock-form.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-stock-form',
   templateUrl: './stock-form.component.html',
-  styleUrls: ['./stock-form.component.scss']
+  styleUrls: ['./stock-form.component.scss'],
 })
 export class StockFormComponent implements OnInit {
+  dollarsVsShares: boolean = true;
+  transaction: string = '';
+  format: string = 'Shares';
+  amount!: string;
+  symbol!: string;
+  finalForm: boolean = false;
+  summary: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  switchFormat(format: string, bool: boolean) {
+    this.format = format;
+    this.dollarsVsShares = bool;
   }
 
+  onSelect(transaction: string) {
+    this.transaction = transaction;
+    this.finalForm = true;
+    
+
+    
+  }
 }
